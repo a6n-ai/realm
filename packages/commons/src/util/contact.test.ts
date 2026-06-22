5080f06ec6eb0060cefb6d45faa368100c59db6e
lawbringr <vishwassaini32@gmail.com>
2026-06-22 22:30:36 +0530

    feat(commons): phone validation primitive with E.164 transform

--- packages/commons/src/util/contact.test.ts
+++ packages/commons/src/util/contact.test.ts
@@ -15,3 +15,23 @@ describe("normalizeEmail", () => {
     expect(normalizeEmail("  Foo@Bar.COM ")).toBe("foo@bar.com");
   });
 });
+
+import { phoneSchema } from "./contact";
+
+describe("phoneSchema", () => {
+  it("accepts a national CA number and outputs E.164", () => {
+    expect(phoneSchema("CA").parse("647 555 0100")).toBe("+16475550100");
+  });
+  it("accepts an already-E.164 number regardless of default country", () => {
+    expect(phoneSchema("IN").parse("+16475550100")).toBe("+16475550100");
+  });
+  it("accepts a national IN number with IN default", () => {
+    expect(phoneSchema("IN").parse("9833098330")).toBe("+919833098330");
+  });
+  it("rejects garbage", () => {
+    expect(phoneSchema("CA").safeParse("12").success).toBe(false);
+  });
+  it("defaults to CA when no country given", () => {
+    expect(phoneSchema().parse("647 555 0100")).toBe("+16475550100");
+  });
+});
